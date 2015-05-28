bf4e461b4294712837eca33faec125eeacb8692d
DelvyCJX <delvy056@hotmail.com>
2015-05-28 18:36:20 -0400

    subi otro

---
+++ El otro IBCS/src/El_otro_IBCS/Properties/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace El_otro_IBCS
+{
+    public class AppSettings
+    {
+        public string SiteTitle { get; set; }
+    }
+}
