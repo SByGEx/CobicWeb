dc8485d6aeae9386eaace41721bf713b2be319d3
emt38 <ezequieltheboss64@gmail.com>
2015-05-28 18:57:11 -0400

    'HOLQ

--- El otro IBCS/src/El_otro_IBCS/Persona.cs
+++ El otro IBCS/src/El_otro_IBCS/Persona.cs
@@ -5,8 +5,65 @@ using System.Threading.Tasks;
 
 namespace El_otro_IBCS
 {
+    enum EstadoCivil { Casado, Soltero, Divorciado, UnionLibre, Viudo }
+
     public class Persona
     {
         private DateTime edad;
+        private string nombre;
+        private string apellido;
+        private EstadoCivil estadoCivi;
+
+        public DateTime Edad
+        {
+            get
+            {
+                return edad;
+            }
+
+            set
+            {
+                edad = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public string Apellido
+        {
+            get
+            {
+                return apellido;
+            }
+
+            set
+            {
+                apellido = value;
+            }
+        }
+
+        internal EstadoCivil EstadoCivi
+        {
+            get
+            {
+                return estadoCivi;
+            }
+
+            set
+            {
+                estadoCivi = value;
+            }
+        }
     }
 }
